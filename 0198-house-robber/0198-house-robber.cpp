9ae84ab1913a258da465fa0740e08ce85d063059
PATEL RAJ PRAVINKUMAR <150596387+Rajpatel637@users.noreply.github.com>
2026-07-10 15:24:45 +0530

    Time: 0 ms (100%), Space: 10.2 MB (78.69%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        int prev2 = 0;
+        int prev1 = nums[0];
+
+        for(int i = 1; i < n;i++){
+            int include = prev2 + nums[i];
+            int exclude = prev1 + 0;
+
+            int ans = max(include,exclude);
+            
+            prev2 = prev1;
+            prev1 = ans;
+        }
+
+        return prev1;
+    }
+};
