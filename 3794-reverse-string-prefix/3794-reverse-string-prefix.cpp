4fc69ac131b59e3d85579a13c8cc48678c9ddb22
PATEL RAJ PRAVINKUMAR <150596387+Rajpatel637@users.noreply.github.com>
2026-08-12 18:15:54 +0530

    Time: 0 ms (100%), Space: 9 MB (19.24%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+
+private:
+    
+    
+
+public:
+    string reversePrefix(string s, int k) {
+
+        int i = 0;
+        int j = k-1;
+
+        while(i < j){
+            swap(s[i],s[j]);
+            i++;
+            j--;
+        }
+
+        return s;    
+    }
+};
